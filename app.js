bf2b4992e1a8f19cbb874f20a21ff6860a188aa6
swathi sb <swathi.sb16@gmail.com>
2023-05-03 16:50:17 +0530

    added function to add the dob

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
-const checkNumberButton= document.querySelector("#check-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+const privacyPolicyBtn = document.querySelector("#privacy-button")
+const privacyNote = document.querySelector(".privacy-policy");
 
-// to add number
-function checkBirthdayDateIsLucky(){
-    const dob = dateOfBirth.values;
-    const sum = calculateSum(dob);
-    console.log(sum);
+
+function compareValues(sum, luckyNumber){
+
+    if (dateOfBirth.value  === "" || luckyNumber === "") {
+        return outputBox.innerText = "Please enter both the details"
+    }else if (Number(luckyNumber < 0)) {
+        return outputBox.innerText = "Please enter positive number"
+    } else if (sum % luckyNumber === 0) {
+        outputBox.innerText = "Yeyy... Your birthday date is lucky 🎂🥳";
+    } else {
+        outputBox.innerText = "Ohh..! Your birthday date is not so lucky 🙁";
+    }
 }
 
-function calculateSum(){
-    dob=dob.replaceAll("-","");
-    sum=0;
-    for(let index=0; index=dob.length; index++){
-        sum=sum+Number(dob.charAt(index));
+function checkBirthdayDateIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    compareValues(sum, luckyNumber.value)
+}
+//to add numbers and then return it
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (let index = 0; index < dob.length; index++) {
+        sum = sum + Number(dob.charAt(index));
     }
     return sum;
 }
 
-checkNumberButton.addEventListener('click', function getValues(){
-    console.log(dateOfBirth.value, luckyNumber.value)
-})
+checkNumberButton.addEventListener('click', checkBirthdayDateIsLucky)
+
+privacyPolicyBtn.addEventListener("click", () => {
+    privacyNote.style.visibility = "hidden";
+});
